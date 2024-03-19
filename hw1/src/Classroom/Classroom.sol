94174078ab34948d7f979b0c30cca945fe8a44ca
Peter <21809peter940324@gmail.com>
2024-03-19 16:24:00 +0800

    commit

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,16 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
+    uint256 has = 0;
 
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+        if(has == 0){
+            has = 1;
+            return 1000;
+        }else{
+            return 123;
+        }
     }
 }
 
